ec74c1de67f5b9e8eef3558342d0ddec1e02debc
fiqih16 <fiqih1666@gmail.com>
2021-12-02 20:05:55 +0700

    View Login & Register

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -46,3 +48,8 @@ Route::get('/categories', function() {
         'categories' => Category::all()
     ]);
 });
+
+// Login
+Route::get('/login', [LoginController::class, 'index']);
+// Register
+Route::get('/register', [RegisterController::class, 'index']);
